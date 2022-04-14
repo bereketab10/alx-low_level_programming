768d8d6676e7b085f58d2b517036c3e922f5347b
bereketab10 <bereketabberhanu10@gmail.com>
2022-04-14 14:13:19 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,18 +22,21 @@ if (i % 3 == 0 && i % 5 != 0)
 
 printf(" Fizz");
 
-} else if (i % 5 == 0 && i % 3 != 0)
+}
+else if (i % 5 == 0 && i % 3 != 0)
 
 {
 
 printf(" Buzz");
 
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
 
 printf(" FizzBuzz");
 
-} else if (i == 1)
+}
+else if (i == 1)
 
 {
 
