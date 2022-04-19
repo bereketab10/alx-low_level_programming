6c51683b610a70776e311c720fa84b3263092705
bereketab10 <bereketabberhanu10@gmail.com>
2022-04-19 14:16:38 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,49 @@
-#include "main.h"
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/**
- */
-int main(void)
-{
-	int num;
+#include <stdlib.h>
 
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
-	return (0);
+#include <time.h>
+
+#include <math.h>
+
+
+
+/**
+ * main - print password.
+ * Return: 0.
+ */
+
+
+
+int main(void)
+
+{
+		int ascii = 2772, i = 0, j, random;
+			char password[100];
+				time_t t;
+					srand((int) time(&t));
+
+						while (ascii > 126)
+
+								{
+		random = rand() % 126;
+													password[i] = random;
+															ascii -= random;
+																	i++;														}
+							if (ascii > 0)
+
+										password[i] = ascii;
+
+								else
+
+										{
+													i--;
+														}
+
+
+									for (j = 0; j <= i; j++)
+											{
+														printf("%c", password[j]);
+															}
+										return (0);
 }
