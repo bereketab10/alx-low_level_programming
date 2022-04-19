7481a2aa4d9922175b184613f7c3dc1c7c86f24c
bereketab10 <bereketabberhanu10@gmail.com>
2022-04-19 14:01:31 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,9 @@
 void rev_string(char *s)
 
 {
-
 	char *start_c, *end_c, c;
-
 	int i, count;
-
 	int length = 0;
-
-
 
 	for (i = 0; s[i]; i++)
 
@@ -28,38 +23,25 @@ void rev_string(char *s)
 
 	}
 
-
 	count = length;
 
-
 	start_c = s;
-
 	end_c = s;
-
 
 	for (i = 0; i < count - 1; i++)
 
 	{
-
 		end_c++;
-
 	}
-
 
 	for (i = 0; i < count / 2; i++)
 
 	{
-
 		c = *end_c;
-
 		*end_c = *start_c;
-
 		*start_c = c;
-
 		start_c++;
-
 		end_c--;
-
 	}
 
 }
