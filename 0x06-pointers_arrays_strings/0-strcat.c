ace32f66f4bfbfbff81c9640389cbfee76236114
bereketab10 <bereketabberhanu10@gmail.com>
2022-04-21 12:49:26 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  *_strcat - concatinate 2 strings.
  *@dest: first string.
@@ -7,23 +9,40 @@
  *Return: string.
  */
 
+
+
 char *_strcat(char *dest, char *src)
 
 {
+
 		int i = 0, j = 0;
 
+
+
 			while (dest[i] != '\0')
+
 						i++;
 
-				while (src[j] != '\0')
-				{
 
-					dest[i] = src[j];
-					i++;
-					j++;					 
-				}
+
+				while (src[j] != '\0')
+
+						{
+
+									dest[i] = src[j];
+
+											i++;
+
+													j++;
+
+														}
+
+
 
 					dest[i] = '\0';
 
+
+
 						return (dest);
+
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-ar *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 
 int _putchar(char c);
 
