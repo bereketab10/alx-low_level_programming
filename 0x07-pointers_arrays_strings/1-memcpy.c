9bf3897bc1972667ba5785da2bc41210c7d0de6a
bereketab10 <bereketabberhanu10@gmail.com>
2022-04-26 03:13:28 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,14 @@
  *Return: dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)  
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;  
-i = 0;  
+unsigned int i;
+i = 0;
 while (i < n)  /*Delacring WHILE*/
-{      
+{
 *(dest + i) = *(src + i); /*add 1 position dest and src*/      
 i++;
 } /*END WHILE*/
-return (dest); 
+return (dest);
 }
